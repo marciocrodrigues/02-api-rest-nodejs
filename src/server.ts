a8a5a43f9ad114907f2466cd3171e1ba51c09e1b
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-09-07 23:08:14 -0300

    alteracao de migrations e teste de rota com consulta no banco

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async (req, res) => {
-  const tables = await knex('transactions').select('*')
-  return tables
+  // const transaction = await knex('transactions').insert({
+  //   id: crypto.randomUUID(),
+  //   title: 'Transação de teste',
+  //   amount: 1000
+  // }).returning('*')
+
+  const transactions = await knex('transactions').select('*').where('amount', 1000)
+
+  return transactions
 })
 
 app
