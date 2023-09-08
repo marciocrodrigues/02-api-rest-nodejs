debd6c6a9b9a8d2f5cdeb87268780b7da8efeb6a
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-09-08 11:50:30 -0300

    criacao de rotas e tipagem do knex

---
+++ src/routes/transactions.ts
@@ -0,0 +1,25 @@
+import crypto from 'node:crypto'
+import { knex } from '../database'
+import { z } from 'zod'
+import { FastifyInstance } from "fastify"
+
+export async function transactionsRoutes(app: FastifyInstance) {
+    app.post('/', async (request, response) => {
+        const createTransactionBodySchema = z.object({
+            title: z.string(),
+            amount: z.number(),
+            type: z.enum(['credit', 'debit'])
+        })
+
+        const { title, amount, type } = createTransactionBodySchema.parse(request.body)
+
+        await knex('transactions').insert({
+            id: crypto.randomUUID(),
+            title,
+            amount: type === 'credit' ? amount : amount * -1
+        })
+
+
+        return response.status(201).send()
+    })
+}
