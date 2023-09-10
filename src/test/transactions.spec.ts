34c24d5560a3240e3ba8b3142935cf8256ace226
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-09-10 01:39:38 -0300

    criacao e configuracao de ambiente de testes e criacao dos testes

--- src/test/transactions.spec.ts
+++ src/test/transactions.spec.ts
@@ -1,4 +1,5 @@
-import { expect, test, beforeAll, afterAll, describe, it } from 'vitest'
+import { expect, test, beforeAll, afterAll, describe, it, beforeEach } from 'vitest'
+import { execSync } from 'node:child_process'
 import request from 'supertest'
 import { app } from '../app'
 
@@ -15,9 +16,13 @@ describe('Transactions routes', () => {
         await app.close()
     })
 
+    beforeEach(() => {
+        execSync('npm run knex migrate:rollback --all')
+        execSync('npm run knex migrate:latest')
+    })
 
     // it ou test é só questão de semantica, mas fazem a mesma coisa
-    it('user can create a new transaction', async () => {
+    it('should be able to create a new transaction', async () => {
         const response = await request(app.server)
             .post('/transactions')
             .send({
@@ -26,5 +31,31 @@ describe('Transactions routes', () => {
                 type: 'credit'
             })
             .expect(201)
+    })
+
+    // testes não devem depender de outros, no caso da listagem precisa ser criado uma transaction, então deve-se criar dentro desse teste
+    it('should be able to list all transactions', async () => {
+        const createTransactionResponse = await request(app.server)
+            .post('/transactions')
+            .send({
+                title: 'New Transaction',
+                amount: 5000,
+                type: 'credit'
+            })
+            .expect(201)
+
+        const cookies = createTransactionResponse.get('Set-Cookie')
+
+        const listTransactionsResponse = await request(app.server)
+            .get('/transactions')
+            .set('Cookie', cookies)
+            .expect(200)
+
+        expect(listTransactionsResponse.body.transactions).toEqual([
+            expect.objectContaining({
+                title: 'New Transaction',
+                amount: 5000
+            })
+        ])
     })
 })
